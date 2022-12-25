37bb38db60d453d1afaceae520f5c77d3df47b5b
nufskiy <nufskiy@rambler.ru>
2022-12-25 19:47:48 +0300

    initial commit - lesson 13

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://placekitten.com/200/200" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://placekitten.com/2000/200" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                    <div>post 3</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
